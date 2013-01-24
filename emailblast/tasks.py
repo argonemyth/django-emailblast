35fd198e655a233d753f0c122836f3ff9a6530ec
Fei Tan <fei@argonemyth.com>
2013-01-24 13:27:11 +0400

    Isolated the email content rendering from the task function.

--- emailblast/tasks.py
+++ emailblast/tasks.py
@@ -29,42 +29,24 @@ class NoContent(Exception):
         return repr(self.value)
 
 
-@task()
-def send_newsletter(email):
+def render_email(context, text_template, html_template=None):
     """
-    The task accepts a newsletter email and send it to all the subscribers.
-    It skip the subscribers we already sent the email to.
+    Render a email given by the template name
     """
-    email.change_status(2)
-    sender = email.newsletter.get_sender()
-    
-    # Get contents ready
-    html_template, text_template = email.newsletter.get_templates()
-    d = Context({ 'email': email })
     try:
         t = loader.get_template(text_template)
-        text_content = t.render(d)
+        text_content = t.render(context)
     except TemplateDoesNotExist:
         text_content = None
 
     if html_template:
         try:
-            """
-            t = loader.get_template(html_template)
-            html_content = t.render(d)
-            """
             html_content = UnicodeSafePynliner().from_string(
-                render_to_string(html_template, {
-                    'email': email,
-                })).run()
+                render_to_string(html_template, context)).run()
         except TemplateDoesNotExist:
             html_content = None
     else:
         html_content = None
-
-    if not text_content and not html_content:
-        #print "None of the following two templates are found: %s and %s" % (text_template, html_template)
-        raise NoContent("None of the following two templates are found: %s and %s" % (text_template, html_template))
 
     """
     if email.image:
@@ -75,6 +57,22 @@ def send_newsletter(email):
         html_content = None
     """
 
+    if not text_content and not html_content:
+        #print "None of the following two templates are found: %s and %s" % (text_template, html_template)
+        raise NoContent("None of the following two templates are found: %s and %s" % (text_template, html_template))
+
+    return text_content, html_content
+
+
+@task()
+def send_newsletter(email):
+    """
+    The task accepts a newsletter email and send it to all the subscribers.
+    It skip the subscribers we already sent the email to.
+    """
+    email.change_status(2)
+    sender = email.newsletter.get_sender()
+    html_template, text_template = email.newsletter.get_templates()
     total_subscribers = str(email.newsletter.subscription.count())
     composer = email.newsletter.get_sender()
     listname = email.newsletter.title
@@ -87,7 +85,12 @@ def send_newsletter(email):
         try:
             email.send_log.get(to__iexact=subscription.email)
         except: 
-            # Prepare the emails:
+            # Construct the email
+            email_context = Context({ 'email': email,
+                                    'recipient': subscription.name })
+            text_content, html_content = render_email(email_context,
+                                                      text_template,
+                                                      html_template)
             if html_content:
                 msg = EmailMultiAlternatives(email.subject, text_content, 
                                              sender, [recipient])
